446f75b2f37da9515ed22f54711d24f859e4a030
Pavel Gric <pavel@ideatech.cz>
2018-08-06 15:34:17 +0200

    better export

--- makeAxiosCall.js
+++ makeAxiosCall.js
@@ -1,8 +1,11 @@
+'use strict';
 import axios from 'axios';
 
-export default (makeCall = async () => {
+const makeCall = async () => {
   const posts = await axios.get(
     'https://jsonplaceholder.typicode.com/posts/42',
   );
   console.log(posts);
-});
+};
+
+export default makeCall;
